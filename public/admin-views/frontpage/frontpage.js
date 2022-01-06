c5c432b6e6fff58cb6948823a921aff936166573
Vibs <vibej@hotmail.com>
2022-01-06 18:33:44 +0100

    defaultbillede tilføjet til visning hvis intet tilknyttet billede

--- public/admin-views/frontpage/frontpage.js
+++ public/admin-views/frontpage/frontpage.js
@@ -30,7 +30,21 @@ function createJewelryView(jewelry){
     jewelryDiv.classList.add("jewelry-row", "align-vert-hor");
 
     jewelryDiv.innerHTML = `
-    <img class="image" alt="${escapeHTML(jewelry.name)}" src="/assets/images/jewelry/${escapeHTML(jewelry.image_path)}">
+
+    ${escapeHTML(jewelry.image_path) 
+        ? `<img class="image" alt="${escapeHTML(jewelry.name)}" src="/assets/images/jewelry/${escapeHTML(jewelry.image_path)}">`
+        : `<img class="image" alt="default_jewelry_image" src="/assets/images/default_jewelry.jpg">`
+
+    
+    
+    
+    }
+
+        
+
+    
+    
+    
     <p class="name">${escapeHTML(jewelry.name)}</p>
     <p class="price"> ${escapeHTML(jewelry.price.toString(10))} dkk</p>
     <p class="stock"> ${escapeHTML(jewelry.stock.toString(10))}</p>
