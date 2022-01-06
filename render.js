50cbc7e164ae15b032cb6df4bd62413da5c0ceea
Vibe Jensen <vibe@sanocast.dk>
2022-01-06 17:18:19 +0100

    createPage tager højde for om det er en admin-side

--- render.js
+++ render.js
@@ -1,20 +1,35 @@
 import fs from "fs";
 
 
-const nav = fs.readFileSync("./public/view-components/navbar/navbar.html", "utf8");
-const footer = fs.readFileSync("./public/view-components/footer/footer.html", "utf8");
+const customerNav = fs.readFileSync("./public/view-components/customer/navbar/navbar.html", "utf8");
+const customerFooter = fs.readFileSync("./public/view-components/customer/footer/footer.html", "utf8");
+
+
+const adminNav = fs.readFileSync("./public/view-components/admin/navbar/navbar.html", "utf8");
+const adminFooter = fs.readFileSync("./public/view-components/admin/footer/footer.html", "utf8");
 
 
 function createPage(path, options) {
     let mainPage;
+    let nav;
+    let footer;
 
-    if(path){
-        mainPage = fs.readFileSync(`./public/views/${path}`, "utf8");
+    if(options?.admin){
+        if(path){
+            mainPage = fs.readFileSync(`./public/admin-views/${path}`, "utf8");
+        }
+        nav = adminNav;
+        footer = adminFooter;
     }
 
+    else if(path){
+        mainPage = fs.readFileSync(`./public/views/${path}`, "utf8");
+        nav = customerNav;
+        footer = customerFooter;
+    }
 
     return (nav + mainPage + footer)
-        .replace("%%DOCUMENT_TITLE%%", options?.title || "Nodefolio")
+        .replace("%%DOCUMENT_TITLE%%", options?.title || "VibeJewelry")
         .replace("%%SCRIPT%%", options?.script ? options.script.map(createScript) : "")
         .replace("%%STYLING%%", options?.styling ? options.styling.map(createStylingLink) : "")//`<link rel="stylesheet" href="/views/${options.styling}"></script>` : "")
         .replace("%%ACTIVE_NAV_LINK%%", options?.activeNavLink);
