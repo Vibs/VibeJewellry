894ac445b4b9766bb2e0338c1af475b35bea193f
Vibe Jensen <vibe@sanocast.dk>
2022-01-15 16:00:32 +0100

    Log ud funktion tilføjet til navbar

--- public/view-components/admin/navbar/navbar.js
+++ public/view-components/admin/navbar/navbar.js
@@ -3,7 +3,21 @@ const returnBtn = document.getElementById('return-btn');
 returnBtn.addEventListener('click', returnToCustomerView);
 
 function returnToCustomerView() {
-    //TODO add logud fra admin-bruger
 
-    window.location.replace("/");
+    fetch("http://localhost:8080/admin/logout", {
+        method: 'DELETE'
+    })
+    .then(response => {
+        if(response.ok){
+            window.location.replace("/");
+        } else {
+            throw new Error(`${response.status} ${response.statusText}`);
+        }
+    })
+    .catch(error => {
+        console.log("Error in logout", error);
+        alert("Der skete en fejl da du forsøgte at logge ud");
+    });
 }
+
+
