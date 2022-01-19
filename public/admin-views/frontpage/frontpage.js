160330b0721e0ed663383cde47e27c88d678def0
Vibs <vibej@hotmail.com>
2022-01-19 13:23:10 +0100

    Ny styling til admin frontpage

--- public/admin-views/frontpage/frontpage.js
+++ public/admin-views/frontpage/frontpage.js
@@ -10,12 +10,12 @@ function createJewelryView(jewelry){
     jewelryDiv.classList.add("jewelry-row", "align-vert-hor");
 
     jewelryDiv.innerHTML = `
-
+    <div class="image">
     ${escapeHTML(jewelry.image_path) 
-        ? `<img class="image" alt="${escapeHTML(jewelry.name)}" src="/assets/images/jewelry/${escapeHTML(jewelry.image_path)}">`
-        : `<img class="image" alt="default_jewelry_image" src="/assets/images/default_jewelry.jpg">`
+        ? `<img class="actual-image " alt="${escapeHTML(jewelry.name)}" src="/assets/images/jewelry/${escapeHTML(jewelry.image_path)}">`
+        : `<img class="actual-image " alt="default_jewelry_image" src="/assets/images/default_jewelry.jpg">`
     }
-
+    </div>
     <p class="name">${escapeHTML(jewelry.name)}</p>
     <p class="price"> ${escapeHTML(jewelry.price.toString(10))} dkk</p>
     <p class="stock"> ${escapeHTML(jewelry.stock.toString(10))}</p>
